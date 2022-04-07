96d7417cbc63fb08ec8bfdf3289e68ea33239c99
Dimitris Boutas <dimitris.boutas@nowmax.be>
2022-04-07 22:22:50 +0200

    student class

---
+++ src/model/Student.java
@@ -0,0 +1,10 @@
+package model;
+
+public class Student {
+
+    private String id;
+    private String name;
+    private String email;
+    private String phoneNumber;
+    private String department;
+}
